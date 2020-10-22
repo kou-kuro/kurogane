ebf33b47b5a8ef330a3e3e95851f5e4c87eab104
kou-kuro <71380718+kou-kuro@users.noreply.github.com>
2020-10-22 17:58:11 +0900

    ユーザー管理

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# User.create!(nickname:  "管理者",
+#   email: "test0@gmail.com",
+#   password:  "02050205",
+#   password_confirmation: "02050205",
+#   firstname: "黒金",
+#   lastname:　"平",
+#   firstname_kana: "クロガネ",
+#   lastname_kana: "ヘイ",
+#   admin: true)
