23dad9712ee5197389de4701e19e03436c3371b7
kou-kuro <71380718+kou-kuro@users.noreply.github.com>
2020-11-06 15:51:06 +0900

    一回目

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,10 @@ require("../index")
 require("../create")
 //= require jquery
 //= require jquery_ujs
+//= require jquery.turbolinks
+//= require turbolinks
 //= require_tree .
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
