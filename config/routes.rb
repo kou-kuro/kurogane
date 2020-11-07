3b240823a6b1f3b03933655b6772da7fcd0a8d8b
kou-kuro <71380718+kou-kuro@users.noreply.github.com>
2020-11-07 14:49:05 +0900

    エラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  post '/callback', to: 'linebot#callback'
+  # post '/callback', to: 'linebot#callback'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
-  # resources :articles, only: [:index, :new, :create, :show]
   resources :articles, only: [:index, :new, :create, :show] do
     collection do 
       get 'business'
       get 'science'
       get 'technology'
     end
-    # resources :favorites, only: [:create, :destroy]
   end
   post 'favorite/:id' => 'favorites#create', as: 'create_favorite'
   delete 'favorite/:id' => 'favorites#destroy', as: 'destroy_favorite' 
 
-  # resources :favorites, only: [:create, :destroy]
   resources :news, only: [:index, :create]
 end
