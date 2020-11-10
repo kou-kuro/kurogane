1f84e1c6c48722e987a4ceb7db05b24970a2d1cc
kou-kuro <71380718+kou-kuro@users.noreply.github.com>
2020-11-10 12:25:53 +0900

    articleテスト

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :article do
-    
+    title { Faker::Lorem.sentence }
+    description { Faker::Lorem.sentence }
+    category_id { 2 }
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
