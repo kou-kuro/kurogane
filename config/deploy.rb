50f7122a11806a79b55b181fa744ce93efed17c9
kou-kuro <71380718+kou-kuro@users.noreply.github.com>
2020-11-10 15:32:14 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,17 +31,3 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 end
-
-set :whenever_roles,        ->{ :app }
-namespace :load do
-  task :defaults do
-    set :whenever_roles,        ->{ :db }
-    set :whenever_command,      ->{ [:bundle, :exec, :whenever] }
-    set :whenever_command_environment_variables, ->{ { rails_env: fetch(:whenever_environment) } }
-    set :whenever_identifier,   ->{ fetch :application }
-    set :whenever_environment,  ->{ fetch :rails_env, fetch(:stage, "production") }
-    set :whenever_variables,    ->{ "environment=#{fetch :whenever_environment}" }
-    set :whenever_update_flags, ->{ "--update-crontab #{fetch :whenever_identifier} --set #{fetch :whenever_variables}" }
-    set :whenever_clear_flags,  ->{ "--clear-crontab #{fetch :whenever_identifier}" }
-  end
-end
